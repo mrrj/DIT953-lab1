ea3d6f0955dd3424d2cc130519be2338575c711a
mrrj <marja.rolandsson@gmail.com>
2020-02-28 12:38:44 +0100

    mvc?

--- DIT953-lab1-master/src/Application.java
+++ DIT953-lab1-master/src/Application.java
@@ -6,12 +6,16 @@ public class Application {
     public static void main(String[] args){
         //TODO: Tie it all together
         CarModel cm = new CarModel();
-        CarView cw = new CarView("CarSim 1.0", cm);
-        CarController cc = new CarController(cm);
+        CarView cw = new CarView("CarSim 1.0",  cm);
+        CarController cc = new CarController(cm, cw);
+
+
 
         cm.cars.add(new Volvo240());
         cm.cars.add(new Saab95());
         cm.cars.add(new Scania());
+
+        cm.addObserver(cw);
 
         //initCars(cc.cars);
 
